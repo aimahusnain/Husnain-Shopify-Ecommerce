106f731d7fddcd07a4f30af97fda73a6b592df13
Muhammad Husnain <aimahusnain@gmail.com>
2024-02-01 15:44:45 -0800

    shopify

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from 'react';
 export const runtime = 'edge';
 
 export const metadata = {
-  description: 'High-performance ecommerce store built with Next.js, Vercel, and Shopify.',
+  description: 'Husnain Ecommerce. The Best Place to Buy Anthing!!',
   openGraph: {
     type: 'website'
   }
